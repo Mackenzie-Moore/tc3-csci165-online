0e0641c2692276335239f6657fb2f81e3ce28df1
Mackenzie Moore <mjm066@mymail.tc3.edu>
2020-01-27 09:05:27 -0500

    lab submission for module 1

---
+++ module-1/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2{
+
+	public static void main(String[] args){
+
+		for (String s : args)
+			System.out.println("Hello " + s + " Moore. Nice work processing the arguments");
+	}
+}
